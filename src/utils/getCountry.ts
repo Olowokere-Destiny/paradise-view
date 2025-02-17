bf1ee051db6f67d7c388d91daa98963d9a89ea03
Olowokere_Destiny <destinyolowokere@gmail.com>
2025-02-17 09:41:31 +0100

    fix

--- src/utils/getCountry.ts
+++ src/utils/getCountry.ts
@@ -1,6 +1,6 @@
 const getUserCountry = async () => {
   try {
-    const response = await fetch("http://ip-api.com/json/");
+    const response = await fetch("https://ip-api.com/json/");
     const data = await response.json();
     return data;
   } catch (error) {
